a3feb58d7c0a772ad7a0f3ab7a2b6ac02514bdae
gigimic <gigimic@gmail.com>
2020-08-25 15:48:55 +0530

    topic model

--- topic_model.py
+++ topic_model.py
@@ -19,16 +19,22 @@ print('Pandas version is : ', pd.__version__)
 name_topics = ["globalization", "mahatma gandhi", "fake news", "women empowerment", "Palliative care", 
 "Auschwitz concentration camp"]
 # name_topics = ["globalization", "fake news", "hollywood"]
-
+# orig_text=[]
 # topic_clean_text=[]
 # for index, name_topic in enumerate(name_topics):
 #     print(index, name_topic)
 #     topic_text = get_text_for_topic(name_topic)
+#     orig_text.append(topic_text)
 #     topic_clean_text.append(clean_text_string(topic_text))
 #     print(topic_clean_text[index][0:100])
 
 # This function can be called to visualise the word cloud of various topics
 # visualize_word_cloud(name_topics, topic_clean_text)
+
+# data_orig = pd.DataFrame({'topics':name_topics, 'text':orig_text})
+# data_orig.to_pickle("data_original.pkl")
+
+data_orig=pd.read_pickle('data_original.pkl')
 
 # data = pd.DataFrame({'topics':name_topics, 'text':topic_clean_text})
 # data.to_pickle("topic_data.pkl")
@@ -61,16 +67,70 @@ data_dtm = pd.read_pickle("dtm_data.pkl")
 # sentiments changing over time
 
 # topic modeling
-tdm = data_dtm.transpose()
-topic_distribution=topic_distrbut(tdm)
+# tdm = data_dtm.transpose()
+# topic_distribution=topic_distrbut(tdm)
+
 # tdm = data_dtm
 
-print('topics are....')
-for entry in topic_distribution:
-    print(entry)
+# print('topics are....')
+# for entry in topic_distribution:
+#     print(entry)
 
 # improve it with including nouns only or nouns and adjectives
 
+# Extract only Ali Wong's text
+top1_text = data_orig.text.loc[1]
+# top1_text = data_corpus.head[0]
+print(top1_text[:200])
+print(data_corpus.topics)
+
+
+from collections import defaultdict
+
+def markov_chain(text):
+    '''The input is a string of text and the output will be a dictionary with each word as
+       a key and each value as the list of words that come after the key in the text.'''
+    
+    # Tokenize the text by word, though including punctuation
+    words = text.split(' ')
+    
+    # Initialize a default dictionary to hold all of the words and next words
+    m_dict = defaultdict(list)
+    
+    # Create a zipped list of all of the word pairs and put them in word: list of next words format
+    for current_word, next_word in zip(words[0:-1], words[1:]):
+        m_dict[current_word].append(next_word)
+
+    # Convert the default dict back into a dictionary
+    m_dict = dict(m_dict)
+    return m_dict
+
+# Create the dictionary for Ali's routine, take a look at it
+top1_dict = markov_chain(top1_text)
+# print(top1_dict)
+
+import random
+
+def generate_sentence(chain, count=100):
+    '''Input a dictionary in the format of key = current word, value = list of next words
+       along with the number of words you would like to see in your generated sentence.'''
+
+    # Capitalize the first word
+    word1 = random.choice(list(chain.keys()))
+    sentence = word1.capitalize()
+
+    # Generate the second word from the value list. Set the new word as the first word. Repeat.
+    for i in range(count-1):
+        word2 = random.choice(chain[word1])
+        word1 = word2
+        sentence += ' ' + word2
+
+    # End it with a period
+    sentence += '.'
+    return(sentence)
+
+gen_text=generate_sentence(top1_dict)
+print(gen_text)
 print('done')
 
 
